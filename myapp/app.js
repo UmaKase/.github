6da0f944f4bc2fd1bd667714721d33df2193e9fa
Chiu <javechiu@gmail.com>
2022-05-16 00:33:32 +0900

    update image to latest version

--- myapp/app.js
+++ myapp/app.js
@@ -9,33 +9,8 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
-console.log(connection.state);
-
+//if console output "2", connect database successfully
 connection.query('select 1+1 as solution', function(err, result,fields){
   if (err) throw err;
   console.log(result[0].solution);
 })
-
-/*
-var express = require('express');
-var app = express();
-console.log('hello')
-app.get('/',function(req, res){
-  var sql = require('mssql');
-  var config = {
-   server : 'mysql-app',	//mysql container name
-   user : 'testuser',
-   password : 'test',
-   database : 'test_database' 		
-  }
-  sql.connect(config, function(err){
-  if(err) console.log(err);
-  console.log('connected')
-    request.query('select * from Student',function (err,recordset){
-      if(err) console.log(err)
-      //res.send(recordset);
-      console.log(recordset)
-    });
-  });
-});
-*/
